996302a2876fd12e6c5123dc7f1fe57009293dc0
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-08-18 15:10:05 -0300

    Reorganizing repo

--- ex2.4/README.md
+++
@@ -1,4 +0,0 @@
-# Exercício
-
-- [ ] Fazer a conta da curva esperada em cima do gráfico obtido com a simulação.
-- [ ] Descobrir como desenhar um gráfico em cima do outro.
--- stuff/send.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-if __name__ == '__main__':
-	for n in range(6):
-		f = 'tardisk0%d' % (n)
-		i = sys.argv[1]
-		p = sys.argv[2]
-		os.system('file2mail.py %s %s %s %s' % (i, p, i, f))
